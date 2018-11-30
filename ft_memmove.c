e37b4a9eee72de5357df74a1d0aaca6c31fa82e8
Vladyslav Horbachenko <vlahorba@e1z3r8p8.42.us.org>
2018-11-30 15:50:54 -0800

    all

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,21 +6,25 @@
 /*   By: vlahorba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 21:16:19 by vlahorba          #+#    #+#             */
-/*   Updated: 2018/11/28 21:17:26 by vlahorba         ###   ########.fr       */
+/*   Updated: 2018/11/30 15:50:19 by vlahorba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*tmp;
+	unsigned char	*d;
+	unsigned char	*s;
 
-	tmp = (char *)malloc(sizeof(char) * len);
-	if (tmp == NULL)
-		return (NULL);
-	ft_memcpy(tmp, src, len);
-	ft_memcpy(dst, tmp, len);
-	free(tmp);
-	return (dst);
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (dst <= src)
+		ft_memcpy(dst, src, len);
+	else
+	{
+		while ((int)len--)
+			d[len] = s[len];
+	}
+	return (d);
 }
